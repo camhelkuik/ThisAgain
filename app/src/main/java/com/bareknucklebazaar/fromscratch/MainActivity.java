7bef431972f595c10a34d96b9e58f6df5025b475
Cameron Helkuik <cameronhelkuik@gmail.com>
2016-12-10 16:32:06 -0600

    trying to sepearte everything out

--- app/src/main/java/com/bareknucklebazaar/fromscratch/MainActivity.java
+++ app/src/main/java/com/bareknucklebazaar/fromscratch/MainActivity.java
@@ -8,31 +8,52 @@ import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    TextView mText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mText = (TextView)findViewById(R.id.knucks_display);
+
     }
 
+    public void loadDataFromAsset() {
+        try {
+            // get input stream for text
+            InputStream is = getAssets().open("randomWordList.txt");
+            // check size
+            int size = is.available();
+            // create buffer for IO
+            byte[] buffer = new byte[size];
+            // get data to buffer
+            is.read(buffer);
+            // close stream
+            is.close();
+            // set result to TextView
+            //mText.setText(new String(buffer));
+            mText.setText(new String(buffer));
+
+        } catch (IOException ex) {
+            return;
+        }
+    }
     //called when user clicks button
     public void showKnucks (View view){
         //show a random knucks
         TextView knucksDisplay = (TextView) findViewById(R.id.knucks_display);
 
-        AssetManager assetManager = getAssets();
-
-        BufferedReader in = new BufferedReader(new FileReader(assetManager.open("/assets/randomWordList.txt")));
-        String str;
-
         List<String> list = new ArrayList<>();
-        while((str = in.readLine()) != null) {
+        while (is != null) {
+            String str = "";
             list.add(str);
         }
         String[] randomKnucks = list.toArray(new String[0]);
