1e6418dba5765099b69e5fa256b25bc8f600d06f
Cameron Helkuik <cameronhelkuik@gmail.com>
2016-12-10 15:12:26 -0600

    showing static string

--- app/src/main/java/com/bareknucklebazaar/fromscratch/MainActivity.java
+++ app/src/main/java/com/bareknucklebazaar/fromscratch/MainActivity.java
@@ -2,6 +2,8 @@ package com.bareknucklebazaar.fromscratch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //called when user clicks button
+    public void showKnucks (View view){
+        //show a random knucks
+        TextView knucksDisplay = (TextView) findViewById(R.id.knucks_display);
+        String randomKnucks = "Punk Cats";
+        knucksDisplay.setText(randomKnucks);
     }
 }
